a33986b55bd2bce2a7070264d6c2a26389c72202
Syed Bakhtawar Fahim <bakhtawarfahim10@gmail.com>
2024-08-26 15:06:48 -0700

    FirstCommit

--- src/data.ts
+++ src/data.ts
@@ -33,12 +33,12 @@ export const menu = [
         url: "/products",
         icon: "product.svg",
       },
-      {
-        id: 3,
-        title: "Orders",
-        url: "/orders",
-        icon: "order.svg",
-      },
+      // {
+      //   id: 3,
+      //   title: "Orders",
+      //   url: "/orders",
+      //   icon: "order.svg",
+      // },
     ],
   },
   // {
